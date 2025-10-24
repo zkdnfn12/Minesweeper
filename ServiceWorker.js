be732825bf53d49e81d82a332ae3394274d7a0eb
zkdnfn12 <53402488+zkdnfn12@users.noreply.github.com>
2025-10-24 13:20:57 +0900

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Minesweeper-My project-0.1.0";
+const contentToCache = [
+    "Build/Wap.loader.js",
+    "Build/Wap.framework.js.unityweb",
+    "Build/Wap.data.unityweb",
+    "Build/Wap.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
